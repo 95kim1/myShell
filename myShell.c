54489df000252e043138d268533fcef334457df8
95kim1 <sunghee.k@kakao.com>
2021-05-30 07:57:28 +0900

    modify - add pipe

--- myShell.c
+++ myShell.c
@@ -4,11 +4,19 @@
 #include <string.h>
 #define MAXARGS 128
 #define MAXPATH 1024
+#define READ 0
+#define WRITE 1
 
 char shellPath[MAXPATH];
 
 /* Function prototypes */
-void eval(char *cmdline);
+void Eval(char *cmdline);
+int parceBgUnit(char cmd[][MAXPATH], char *cmdline);
+
+void Eval2(char *cmdline);
+int parceByPipeline(char cmd[][MAXPATH], char *cmdline);
+
+void eval(char *cmdline, int *flag, int *fd_p2c, int *fd_c2p);
 int parseline(char *buf, char **argv);
 int builtin_command(char **argv);
 void getCmdPath(char *prog, char *path);
@@ -33,14 +41,113 @@ int main()
             exit(0);
 
         /* Evaluate */
-        eval(cmdline);
+        Eval(cmdline);
     }
 }
 /* $end shellmain */
 
+/* separate background commands */
+void Eval(char *cmdline)
+{
+    char cmd[MAXARGS][MAXPATH];
+    int cnt = parceBgUnit(cmd, cmdline);
+    for (int i = 0; i < cnt; i++)
+        Eval2(cmd[i]);
+}
+
+/* "ls | cat & ls" -> "ls | cat",  " ls"*/
+int parceBgUnit(char cmd[][MAXPATH], char *cmdline)
+{
+    int tot_len = strlen(cmdline);
+    int cnt = 0;
+    int j = 0;
+    for (int i = 0; i < tot_len; i++)
+    {
+        cmd[cnt][j++] = cmdline[i];
+
+        if (cmdline[i] == '&')
+        {
+            cmd[cnt][j] = '\0';
+            cnt++;
+            j = 0;
+        }
+    }
+    if (j > 0)
+        cmd[cnt++][j] = '\0';
+    return cnt;
+}
+
+/* separate commands with pipeline */
+void Eval2(char *cmdline)
+{
+    char cmd[MAXARGS][MAXPATH];
+    int cnt = parceByPipeline(cmd, cmdline);
+    int flag[2] = {0, 0}; // flag[0]==1 => parent -> child  flag[1]==1 => child -> parent
+    int fd_p2c[2];        // will be piped to transfer data from parent to child
+    int fd_c2p[2];        // will be piped to transefer data from child to parent
+    //printf("--Eval2: cnt: %d==\n", cnt);
+    for (int i = 0; i < cnt; i++)
+    {
+        //printf("--Eval2: %s, eval==\n", cmd[i]);
+
+        if (i == 0)
+            flag[0] = 0;
+        else if (cnt > 1) /* pipe: parent to child */
+        {
+            flag[0] = 1;
+            pipe(fd_p2c);
+
+            /* read from fd_c2p[READ] and write to fd_p2c[WRITE]*/
+            char line[8912];
+            line[0] = '\0';
+
+            //printf("---before read: %s,,\n===", line);
+            while (read(fd_c2p[READ], line, 8912) > 0)
+            {
+                write(fd_p2c[WRITE], line, strlen(line));
+                //printf("--pipe read: %s,\n==", line);
+            }
+
+            close(fd_c2p[READ]);
+        }
+
+        if (i == cnt - 1)
+            flag[1] = 0;
+        else if (cnt > 1)
+        { /* pipe: child to parent */
+            flag[1] = 1;
+            pipe(fd_c2p);
+        }
+
+        eval(cmd[i], flag, fd_p2c, fd_c2p);
+    }
+}
+
+/* "ls | cat" -> "ls ", " cat" */
+int parceByPipeline(char cmd[][MAXPATH], char *cmdline)
+{
+    int tot_len = strlen(cmdline);
+    int cnt = 0;
+    int j = 0;
+    for (int i = 0; i < tot_len; i++)
+    {
+        cmd[cnt][j++] = cmdline[i];
+
+        if (cmdline[i] == '|')
+        {
+            cmd[cnt][j - 1] = '\0';
+            cnt++;
+            j = 0;
+        }
+    }
+    if (j > 0)
+        cmd[cnt++][j] = '\0';
+    return cnt;
+}
+
 /* $begin eval */
 /* eval - Evaluate a command line */
-void eval(char *cmdline)
+void eval(char *cmdline, int *flag, int *fd_p2c, int *fd_c2p)
 {
     char *argv[MAXARGS]; /* Argument list execve() */
     char buf[MAXLINE];   /* Holds modified command line */
@@ -49,8 +156,10 @@ void eval(char *cmdline)
 
     strcpy(buf, cmdline);
     bg = parseline(buf, argv);
+
     if (argv[0] == NULL)
         return; /* Ignore empty lines */
+
     if (!builtin_command(argv))
     { //quit -> exit(0), & -> ignore, other -> run
         if ((pid = Fork()) == 0)
@@ -58,19 +167,42 @@ void eval(char *cmdline)
             char prog[MAXPATH];
             getCmdPath(prog, argv[0]);
 
+            if (flag[0]) // pipe: parent to child
+            {
+                dup2(fd_p2c[READ], READ); // In child process, stdio indicates file that fd_p2c[READ] is indicating
+                close(fd_p2c[READ]);      // stdio indicates file that fd_p2c[READ] indicating, so close this descriptor
+                close(fd_p2c[WRITE]);     // do not use fd_p2c[WRITE] in child process
+            }
+
+            if (flag[1]) // pipe: child to parent
+            {
+                dup2(fd_c2p[WRITE], WRITE);
+                close(fd_c2p[WRITE]);
+                close(fd_c2p[READ]);
+            }
+
             if (execve(prog, argv, environ) < 0)
-            { //ex) /bin/ls ls -al &
+            {
                 printf("%s: Command not found.\n", argv[0]);
                 exit(0);
             }
             exit(0);
         }
 
+        if (flag[0])
+        {
+            close(fd_p2c[READ]);
+            close(fd_p2c[WRITE]);
+        }
+
+        if (flag[1])
+            close(fd_c2p[WRITE]);
+
         /* Parent waits for foreground job to terminate */
         if (!bg)
         {
             int status;
-            waitpid(pid, &status, 0);
+            Waitpid(pid, &status, 0);
         }
         else //when there is backgrount process!
             printf("%d %s", pid, cmdline);
